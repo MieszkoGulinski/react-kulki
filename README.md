535ac5ccf9f76205e6dd61f0d429835b2663d333
Mieszko Gulinski <mieszko_gulinski@radcode.co>
2020-03-25 16:49:19 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+```
+npm install
+npm run watch
+npm run build
+http-server
+```
+
+Files build/bundle.js and build/style.css are built by Webpack - but they need to be commited to Git, because they need to be available on the
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: ['@babel/polyfill', './src/index.js'],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.(sass|scss|css)$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader']),
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: `${__dirname}/build`,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'style.css',
+    })
+  ],
+};
